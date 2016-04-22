dd053fd92e7b246ca7577d08f0e039bbad1cbecb
Tudor <xenogenesis@openmailbox.org>
2016-04-22 23:50:08 +0300

    dattr: fix warnings

--- config.mk
+++ config.mk
@@ -4,5 +4,5 @@ MANPREFIX = $(PREFIX)/share/man
 CC = cc
 LD = $(CC)
 
-CFLAGS +=  `pkg-config --cflags xcb xcb-randr` -std=c99 -pedantic -Wall -O2
+CFLAGS +=  `pkg-config --cflags xcb xcb-randr` -pedantic -Wall -O2
 LDFLAGS += `pkg-config --libs xcb xcb-randr`
